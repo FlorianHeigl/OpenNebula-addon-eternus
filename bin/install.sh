8e4c3e36d1a4dda8447a3b9cc6a42610af9a0c89
Florian Heigl <me@florianheigl.me>
2021-04-29 04:22:12 +0200

    update bin/install.sh: comments and automatic host listing

--- bin/install.sh
+++ bin/install.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+# exit on any error
+set -o pipefail
 set -e
 
-cp ~deepthink/OpenNebula-addon-eternus/datastore/eternus/* /var/lib/one/remotes/datastore/eternus/
-cp ~deepthink/OpenNebula-addon-eternus/tm/eternus/* /var/lib/one/remotes/tm/eternus/
+# resolve installer path and copy the driver bits from below there
+cp $( dirname $0 )/../datastore/eternus/* /var/lib/one/remotes/datastore/eternus/
+cp $( dirname $0 )/../tm/eternus/* /var/lib/one/remotes/tm/eternus/
 chmod 755 /var/lib/one/remotes/*/eternus/*
-onehost sync 0 --force
-onehost sync 1 --force
+
+# sync the updated driver to a list of all active compute nodes
+# the list is gathered using the ONE cli
+onehost list | grep on| awk '{system("onehost sync "$1" --force")}'
